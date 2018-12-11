e155c8fe31ab418b62abbeea7942d7e8fbae573f
Lokman Hossen <lhmisho@gmail.com>
2018-12-11 23:25:39 +0600

    django session

--- catalog/views.py
+++ catalog/views.py
@@ -11,17 +11,12 @@ def index(request):
     num_books = Book.objects.all().count()
     num_instance = BookInstance.objects.all().count()
 
-    # Available books (status = 'a')
-    num_instance_available = BookInstance.objects.filter(status__iexact='a').count()
-
-    # number of author
-    num_author = Author.objects.all().count()
-
-    # number of genre
-    num_genre = Genre.objects.all().count()
-
-    # num of books in case insensitive
-    num_of_insensetive_books = Book.objects.filter(title__icontains='b')
+    num_instance_available = BookInstance.objects.filter(status__iexact='a').count() # Available books (status = 'a')
+    num_author = Author.objects.all().count()  # number of author
+    num_genre = Genre.objects.all().count()     # number of genre
+    num_of_insensetive_books = Book.objects.filter(title__icontains='b')  # num of books in case insensitive
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
 
     context = {
         'num_books' : num_books,
@@ -30,6 +25,7 @@ def index(request):
         'num_author' : num_author,
         'num_genre'  : num_genre,
         'num_of_insensetive_books' : num_of_insensetive_books,
+        'num_visits' : num_visits,
     }
 
     return render(request, 'index.html', context)
